ecd7aef6364d108ada13ba8c3abf1eaced2ce81d
Seoyoung Cho <cho411@purdue.edu>
2024-03-25 14:19:35 -0400

    day6

---
+++ day6.java
@@ -0,0 +1,15 @@
+/*
+ * day 6
+ * watched up to tutorial 63
+ * 
+ * <What I've learned>
+ * can overwrite with same function name if it extends smth
+ * 
+ * 'abstract' public class -> can't create object from it
+ * public 'abstract' void eat(); -> doesn't need body
+ * you have to overwrite with 'public' void eat() in other files
+ * 
+ * [64]
+ * more GUI stuff
+ *  - how to make icon changes when mouse hovers
+ */
